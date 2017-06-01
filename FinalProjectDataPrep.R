02e256a54772e9f342a4c650fd0b014524a9c5f1
gspinnadauhu <markusalang@gmail.com>
2017-06-01 16:59:38 -0500

    uploading script to extract fires data set

---
+++ FinalProjectDataPrep.R
@@ -0,0 +1,14 @@
+# SQLite Database was downloaded from https://www.fs.usda.gov/rds/archive/Product/RDS-2013-0009.4/ and extracted in ./data
+# Connecting to SQLite file containing data on wildfires in US
+library(tidyverse)
+library(RSQLite)
+connection<-dbConnect(drv=RSQLite::SQLite(),"./data/FPA_FOD_20170508.sqlite")
+tables <- dbListTables(connection)
+## the only table needed is "Fires", writing query for fires
+query<-dbSendQuery(connection, "SELECT * FROM Fires")
+fires<-dbFetch(query,n=-1)
+## closing the connection
+dbClearResult(query)
+dbDisconnect(connection)
+## save fires in ./data as csv, excluding shape column which does not contain useful info
+write.table(fires[,-39],file="./data/fires.csv",sep=",")
