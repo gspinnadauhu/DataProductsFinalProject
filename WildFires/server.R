af7f94830204e083e2a8a5640985b7e5a782c8c0
gspinnadauhu <markusalang@gmail.com>
2017-06-27 17:57:28 -0500

    removed tab layout. will add summary table below map

--- WildFires/server.R
+++ WildFires/server.R
@@ -25,5 +25,6 @@ shinyServer(function(input, output) {
                                          )
         }
         )
+        output$FireSummary<-renderTable()
 }
 )
--- WildFires/ui.R
+++ WildFires/ui.R
@@ -6,32 +6,28 @@ library(tidyverse)
 shinyUI(
     fluidPage(
         # Application title
-        titlePanel("USDA Forestry Service Wild Fire Occurrences 1992-2015"),        
-        tabsetPanel(
-                tabPanel("Map",
-                         sidebarLayout(
-                                 #Side Panels to select Date Range & State
-                                 sidebarPanel(
-                                         checkboxGroupInput("DateInput",
-                                                            "Years",
-                                                            choices=sort(unique(fires$FIRE_YEAR),
-                                                                         decreasing=FALSE),
-                                                            inline=TRUE),
-                                         selectInput("StateInput",
-                                                     "State(s)",
-                                                     sort(unique(fires$STATE),
-                                                          decreasing = FALSE),
-                                                     selected=NULL,
-                                                     multiple = TRUE),
-                                         submitButton("Apply Selection")
-                                 ),
-                                 #Main Panel to display a map with fire locations
-                                 mainPanel(
-                                         leafletOutput("firemap")
-                                 )
-                         )
-                         ),
-                tabPanel("Summary","Summarize & Download Wildfire Data")
+        titlePanel("USDA Forestry Service Wild Fire Occurrences 1992-2015"),
+        sidebarLayout(
+                #Side Panels to select Date Range & State
+                sidebarPanel(
+                        checkboxGroupInput("DateInput",
+                                           "Years",
+                                           choices=sort(unique(fires$FIRE_YEAR),
+                                           decreasing=FALSE),
+                                           inline=TRUE),
+                        selectInput("StateInput",
+                                    "State(s)",
+                                    sort(unique(fires$STATE),
+                                         decreasing = FALSE),
+                                    selected=NULL,
+                                    multiple = TRUE),
+                        submitButton("Apply Selection")
+                        ),
+                #Main Panel to display a map with fire locations
+                mainPanel(
+                        leafletOutput("firemap"),
+                        tableOutput("FireSummary")
+                        )
+                )
         )
     )
-)
